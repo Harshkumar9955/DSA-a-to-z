a26f1022a860865848f494a943585efc83077f3b
Harshkumar9955 <harshsingh04072006@gmail.com>
2026-06-12 20:55:23 +0530

    fix bug

--- function.cpp
+++ function.cpp
@@ -27,9 +27,12 @@
 using namespace std;
 
 int main(){
-    vector<int>& arr;
     
+    
+    int n;
     cin >> n;
+
+    vector<int> arr(n);
     for(int i=0;i<n;i++){
         cin >> arr[i];
     }
@@ -41,7 +44,7 @@ int main(){
     }
 
     for(int i=0;i<n;i++){
-        cout << arr[i];
+        cout << arr[i] << " ";
     }
     return 0;
 
