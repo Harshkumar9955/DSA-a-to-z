ca6bbacd09d6f2d06d37e0eb4f619917751d4173
Harshkumar9955 <harshsingh04072006@gmail.com>
2026-06-16 20:26:08 +0530

    improvement in code

--- Build-up Logical Thinking by Pattern/Pattern7.cpp
+++ Build-up Logical Thinking by Pattern/Pattern7.cpp
@@ -19,12 +19,10 @@ void Pattern7(int n){
         for(int j=0;j<n-i-1;j++){
             cout << " ";
         }
-        for(int k=0;k<2*i+1;k++){
+        for(int j=0;j<2*i+1;j++){
             cout << "*";
         }
-        for(int l=0;l<n-i-1;l++){
-            cout << " ";
-        }
+        
 
         cout << endl;
     }
